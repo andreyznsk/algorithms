d259f6cdd91854ee9ef8f60e01f08e611e93d6b7
andreyznsk <andreyznsk@gmail.com>
2021-02-15 17:49:59 +0700

    1

--- src/main/java/lesson3/DQueueImpl.java
+++ src/main/java/lesson3/DQueueImpl.java
@@ -3,23 +3,26 @@ package lesson3;
 public class DQueueImpl<E> extends QueueImpl<E> implements DQueue<E> {
 
     public DQueueImpl(int maxSize) {
-
         super(maxSize);
     }
 
     @Override
     public boolean insertLeft(E value) {
-
         if (isFull()) {
             return false;
         }
+        if(head==DEFAULT_HEAD)
+            head = data.length;
 
-        if(size == 0) {
+        data[--head] = value;
+        size++;
+        return true;
+
+       /* if(size == 0) {
             data[head] = value;
             size++;
             tail = DEFAULT_HEAD;
             return true;
-
         }
 
         //Проверка если из дек удалялись элемнты слева и голова указывает не на 0 элемент массива
@@ -46,7 +49,7 @@ public class DQueueImpl<E> extends QueueImpl<E> implements DQueue<E> {
         }
 
         size++;
-        return true;
+        return true;*/
     }
 
     @Override
@@ -60,11 +63,9 @@ public class DQueueImpl<E> extends QueueImpl<E> implements DQueue<E> {
         if (isEmpty()) {
             return null;
         }
-
         if (tail == -1) {
             tail = data.length-1;
         }
-
         E removedValue = data[tail];
         data[tail--] = null;
         size--;
@@ -85,13 +86,13 @@ public class DQueueImpl<E> extends QueueImpl<E> implements DQueue<E> {
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder("Текущий ДЕК: [");
-            for (int i = 0; i < size - 1; i++) {
+            if (this.size == 0) return "Дек пустой";
+
+            for (int i = 0; i < data.length; i++) {
                 sb.append(data[i]);
                 sb.append(", ");
             }
-            if (size > 0) {
-                sb.append(data[size - 1]);
-            }
+
             sb.append("]");
 
             return sb.toString();
--- src/main/java/lesson3/DQueueTest.java
+++ src/main/java/lesson3/DQueueTest.java
@@ -5,23 +5,17 @@ public class DQueueTest {
     private static DQueue<Integer> dQueue = new DQueueImpl<>(MAX_SIZE);
 
     public static void main(String[] args) {
-
-
-        System.out.println("Вставляем в пусой список слева: " + dQueue.insertLeft(-1));
-        System.out.println(" "+dQueue.peekHead());
-
+        System.out.println("Вставляем в пусой дек слева: " + dQueue.insertLeft(-1));
+        //System.out.println(" "+dQueue.peekHead());
+        dQueue.display();
         for (int i = 0; i < MAX_SIZE-5; i++) {
             System.out.println("Вставляем элемент: " + i + " Результат: " + dQueue.insertRight(i));
         }
-
         System.out.println("Вставляем в дек слева: " + dQueue.insertLeft(-2));
 
-
         dQueue.display();
-
         System.out.println("Извлекаем элемент слева: " + dQueue.removeLeft());
         dQueue.display();
-
         System.out.println("Вставляем в дек слева: " + dQueue.insertLeft(-2));
         dQueue.display();
         System.out.println("Вставляем в дек слева: " + dQueue.insertLeft(-2));
@@ -29,21 +23,15 @@ public class DQueueTest {
         System.out.println("Вставляем в дек слева: " + dQueue.insertLeft(-2));
         System.out.println("Вставляем в дек слева: " + dQueue.insertLeft(-2));
         dQueue.display();
-
         System.out.println("Удалем элмент справа: " + dQueue.removeRight());
         dQueue.display();
-
         System.out.println("Удалем элмент справа: " + dQueue.removeRight());
         dQueue.display();
-
         System.out.println("Удалем элмент справа: " + dQueue.removeRight());
         dQueue.display();
-
-
         System.out.print("Вывод вссего ДЕК справа налево: ");
         while (!dQueue.isEmpty())
         System.out.print(dQueue.removeRight() + " ");
-
       /*
         while (!dQueue.isEmpty())
             System.out.print(dQueue.removeLeft() + " ");*/
